475a7f5d850a81fbfe66b4997970aa8b88704017
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-09-25 18:35:19 -0300

    feat: Adding first terminal screen

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
+package main
 
-
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+)
 
 func main() {
-	
+	a := app.New()	
+	w := a.NewWindow("simple terminal")
+
+	ui := widget.NewTextGrid()
+
+	ui.SetText("Hello terminal")
+
+	w.SetContent(
+		fyne.NewContainerWithLayout(
+			layout.NewGridWrapLayout(fyne.NewSize(420, 200)),
+			ui,
+		),
+	)
+	w.ShowAndRun()
 }
